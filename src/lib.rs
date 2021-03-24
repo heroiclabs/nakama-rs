872b6248cd433c0899a3641241dce6f4ef586c64
Fedor Logachev <not.fl3@gmail.com>
2021-03-24 16:36:52 -0600

    RT-api for web

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ pub mod api {
 
 pub mod rt_api {
     use nanoserde::DeJson;
-    use std::sync::mpsc::{self, channel};
+    use quad_net::web_socket::WebSocket;
 
     #[derive(DeJson, Debug)]
     pub struct EventContainer {
@@ -38,6 +38,7 @@ pub mod rt_api {
     pub struct MatchData {
         pub match_id: String,
         pub presence: Presence,
+        #[nserde(default)]
         #[nserde(proxy = "Base64Encoder")]
         pub data: Vec<u8>,
         pub op_code: String,
@@ -66,43 +67,8 @@ pub mod rt_api {
         }
     }
 
-    struct Client {
-        out: ws::Sender,
-        thread_out: mpsc::Sender<Event>,
-    }
-
-    enum Event {
-        Connect(ws::Sender),
-        Message(String),
-    }
-
     pub struct Socket {
-        sender: ws::Sender,
-        rx: mpsc::Receiver<Event>,
-    }
-
-    impl ws::Handler for Client {
-        fn on_open(&mut self, _: ws::Handshake) -> ws::Result<()> {
-            self.thread_out
-                .send(Event::Connect(self.out.clone()))
-                .unwrap();
-            Ok(())
-        }
-
-        fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
-            self.thread_out
-                .send(Event::Message(msg.into_text().unwrap()))
-                .unwrap();
-            Ok(())
-        }
-
-        fn on_close(&mut self, code: ws::CloseCode, _reason: &str) {
-            println!("closed {:?}", code);
-        }
-
-        fn on_error(&mut self, error: ws::Error) {
-            println!("{:?}", error);
-        }
+        web_socket: WebSocket,
     }
 
     impl Socket {
@@ -112,111 +78,38 @@ pub mod rt_api {
                 addr, port, appear_online, token
             );
 
-            let (tx, rx) = channel();
-            std::thread::spawn(move || {
-                ws::connect(ws_addr, |out| Client {
-                    out,
-                    thread_out: tx.clone(),
-                })
-                .unwrap()
-            });
-
-            match rx.recv() {
-                Ok(Event::Connect(sender)) => Socket { sender, rx },
-                _ => panic!("Failed to connect websocket"),
+            Socket {
+                web_socket: WebSocket::connect(&ws_addr).unwrap(),
             }
+        }
+
+        pub fn connected(&self) -> bool {
+            self.web_socket.connected()
         }
 
         pub fn try_recv(&mut self) -> Option<String> {
-            self.rx.try_recv().ok().map(|event| match event {
-                Event::Message(msg) => msg,
-                _ => panic!(),
-            })
+            self.web_socket
+                .try_recv()
+                .map(|bytes| String::from_utf8(bytes).unwrap())
         }
 
-        pub fn join_match(&self, match_id: &str) {
-            self.sender
-                .send(format!(
-                    r#"{{"match_join":{{"match_id":"{}"}},"cid":"1"}}"#,
-                    match_id
-                ))
-                .unwrap();
+        pub fn join_match(&mut self, match_id: &str) {
+            self.web_socket.send_text(&format!(
+                r#"{{"match_join":{{"match_id":"{}"}},"cid":"1"}}"#,
+                match_id
+            ));
         }
 
-        pub fn match_data_send(&self, match_id: &str, opcode: i32, data: &[u8]) {
+        pub fn match_data_send(&mut self, match_id: &str, opcode: i32, data: &[u8]) {
             let mut buf = String::new();
             base64::encode_config_buf(data, base64::STANDARD, &mut buf);
 
-            self.sender
-                .send(format!(
+            self.web_socket
+                .send_text(&format!(
                     r#"{{"match_data_send":{{"match_id":"{}","op_code":"{}","data":"{}","presences":[]}}}}"#,
                     match_id, opcode, buf
-                ))
-                .unwrap();
+                ));
         }
-    }
-}
-
-pub mod sync_client {
-    use super::api;
-
-    pub fn make_request<T: nanoserde::DeJson>(
-        server: &str,
-        port: u32,
-        request: api::RestRequest<T>,
-    ) -> T {
-        let auth_header = match request.authentication {
-            api::Authentication::Basic { username, password } => {
-                format!(
-                    "Basic {}",
-                    base64::encode(&format!("{}:{}", username, password))
-                )
-            }
-            api::Authentication::Bearer { token } => {
-                format!("Bearer {}", token)
-            }
-        };
-        let method = match request.method {
-            api::Method::Post => ureq::post,
-            api::Method::Put => ureq::put,
-            api::Method::Get => ureq::get,
-            api::Method::Delete => ureq::delete,
-        };
-
-        let response: String = method(&format!(
-            "{}:{}{}?{}",
-            server, port, request.urlpath, request.query_params
-        ))
-        .set("Authorization", &auth_header)
-        .send_string(&request.body)
-        .unwrap()
-        .into_string()
-        .unwrap();
-
-        nanoserde::DeJson::deserialize_json(&response).unwrap()
-    }
-
-    #[test]
-    fn auth() {
-        let request = api::authenticate_email(
-            "defaultkey",
-            "",
-            api::ApiAccountEmail {
-                email: "super3@heroes.com".to_string(),
-                password: "batsignal2".to_string(),
-                vars: std::collections::HashMap::new(),
-            },
-            Some(false),
-            None,
-        );
-
-        let response = make_request("http://127.0.0.1", 7350, request);
-        println!("{:?}", response);
-
-        println!("connecting socket");
-        let nakama_socket =
-            crate::rt_api::Socket::connect("ws://127.0.0.1", 7350, false, &response.token);
-        nakama_socket.join_match("asd");
     }
 }
 
